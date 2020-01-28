afad963728cd38563f1eb76f7b99b3e57351b5c7
fbmlj <lucastavasousa@gmail.com>
2020-01-27 21:36:19 -0300

    Login pronto

--- start/routes.js
+++ start/routes.js
@@ -20,3 +20,4 @@ Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 Route.post('/users/sign_up','UserController.create')
+Route.post('/users/sign_in', 'SessionController.create')
