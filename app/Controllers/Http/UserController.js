99fd6e0601a1ca328164384cfcb33f13f35a80cc
fbmlj <lucastavasousa@gmail.com>
2020-02-01 19:12:54 -0300

    adicionando validator

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,7 +5,8 @@ const User = use("App/Models/User")
 class UserController {
   async create ({ request }) {
     const data = request.only(["name", "email", "password"])
-    const valid = await User.findByOrFail("email", data.email)
+    
+    const valid = await User.findBy("email", data.email)
     if (!valid)
       return User.create(data)
       
